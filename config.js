b6643dd052c5c69322a24c73a82d750032f60a3a
Duylinhdntu <148708476+Duylinhdntu@users.noreply.github.com>
2024-05-06 21:59:08 +0700

    Update config.js
    Yêu em

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Bé yêu của Dilinh",
+    introTitle: 'Cục cưng hay dỗi của Dilinh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Cục cưng đừng dổi anh nữa mà🥰',
+    desc: 'Tha lỗi cho anh nhó ',
+    btnYes: 'Dạ <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'Bạn hết dỗi òi đún hum',
+    btnAccept: 'Dạ <3',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/6856222157837662' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
